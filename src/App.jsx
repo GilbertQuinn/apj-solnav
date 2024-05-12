4b6e1777110061995249ceee7a1b53a59abe174c
GilbertQuinn <gilbertq@gmail.com>
2024-05-13 10:26:24 +1200

    Debugging

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
 
       //Check if we have a JWT cookie
       const token = getToken();
-      console.log("Token:", token)
+      console.log("Page Load Token:", token)
 
       if (token && !isTokenExpired(token)) {
         setShowAdmin(true)
